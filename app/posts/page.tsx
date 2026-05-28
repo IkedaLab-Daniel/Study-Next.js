16c9862d018eb11630bf349f9ebde1b264e0c785
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-05-29 05:04:37 +0800

    dynamic, challenge 1

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,9 +1,31 @@
+import Link from "next/link"
+
 export default async function PostsPage() {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts")
-  const data = await res.json()
+  const posts = await res.json()
+
+  /**
+   * Challenge:
+   * Add a Link to each blog post (surrounding the `h2` elements) that leads
+   * to {`/posts/${post.id}`}
+   * 
+   * Note: these will lead to non-existing pages. We'll fix that next ⏳
+   */
+
   return (
-    <pre>
-      {JSON.stringify(data, null, 2)}
-    </pre>
+    <div className="posts-container">
+      <h1>Blog Posts</h1>
+      <ul className="posts-list">
+        {posts.map((post) => (
+          <li key={post.id} className="post-item">
+            <h2>
+              <Link href={`post/${post.id}`}>
+                {post.title}
+              </Link>
+            </h2>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
