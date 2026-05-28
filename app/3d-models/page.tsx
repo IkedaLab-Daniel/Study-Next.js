bf250a6a60a2cf08ec562011133f4a0e8d67ee8e
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-05-29 04:53:48 +0800

    minimal styling

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -5,23 +5,24 @@ export default async function Models() {
     const res = await getAllModels()
 
     return(
-        <>
+        <div className="flex flex-col gap-2 items-center">
             {res.map((item, index) => (
-                <div key={index}>
+                <div key={index} className="w-100 border-2 rounded-md p-4">
                     <Image 
                         src={item.image}
-                        width={50}
-                        height={50}
+                        width={600}
+                        height={750}
                         alt="item image"
+                        className="rounded-md"
                     />
-                    <h2>{item.name}</h2>
-                    <p>{item.description}</p>
-                    <p>{item.likes}</p>
+                    <h2 className="font-bold text-2xl my-2">{item.name}</h2>
+                    <p className="mb-2">{item.description}</p>
+                    <p>Likes: {item.likes}</p>
                     <p>Category: {item.category}</p>
                     <p>Date added: {item.dateAdded}</p>
                 </div>
             ))}
-        </>
+        </div>
     )
 
 }
