f690ff0327ebdf48dee15230e25666d2c3961d37
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-06-02 04:36:24 +0800

    sync challenge solution

--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -1,16 +1,14 @@
-"use client"
 import Link from "next/link";
 import { NavLinkProps } from "../types";
-import { usePathname } from "next/navigation";
 
-export default function NavLink({ href, children} : NavLinkProps) {
+export default function NavLink({ href, children, isActive} : NavLinkProps) {
 
     return(
         <li className="text-sm uppercase">
             <Link
                 href={href}
                 className={`px-4 py-2 text-gray-700 transition-colors rounded-md cursor-pointer hover:text-orange-accent 
-                    ${usePathname() === href ? "text-orange-accent" : ""}
+                    ${isActive ? "text-orange-accent" : ""}
                 `}
             >
                 {children}
