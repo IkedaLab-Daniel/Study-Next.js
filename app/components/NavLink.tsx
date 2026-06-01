9faf89803c2900e66752decf7124a61a1e1094d8
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-06-02 04:22:32 +0800

    rendering strategies challenge completed

--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -1,12 +1,17 @@
+"use client"
 import Link from "next/link";
 import { NavLinkProps } from "../types";
+import { usePathname } from "next/navigation";
 
 export default function NavLink({ href, children} : NavLinkProps) {
+
     return(
         <li className="text-sm uppercase">
             <Link
                 href={href}
-                className="px-4 py-2 text-gray-700 transition-colors rounded-md cursor-pointer hover:text-orange-accent"
+                className={`px-4 py-2 text-gray-700 transition-colors rounded-md cursor-pointer hover:text-orange-accent 
+                    ${usePathname() === href ? "text-orange-accent" : ""}
+                `}
             >
                 {children}
             </Link>
