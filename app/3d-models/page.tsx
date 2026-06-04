860772e5b3866c1869960d6d46cc0f9db0e29693
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-06-05 04:39:28 +0800

    search feature on 3d models

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -1,7 +1,28 @@
 import ModelsGrid from "@/app/components/ModelsGrid"
 import { getModels } from "@/app/lib/models"
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
     const models = await getModels()
-    return <ModelsGrid title="3D Models" models={models} />
+    const {query} = await searchParams
+
+    const filteredModel = query
+        ? models.filter(obj => obj.name.toLocaleLowerCase().includes(query.toLocaleLowerCase()) || obj.description.toLocaleLowerCase().includes(query.toLocaleLowerCase()))
+        : models;
+
+    console.log(filteredModel.length)
+
+    return (
+        <>
+            <form>
+                <label htmlFor="query">Search 3D Model: </label>
+                <input 
+                    type="text" 
+                    name="query" 
+                    placeholder="e.g. Dragon"
+                    defaultValue={query}
+                />
+            </form>
+            <ModelsGrid title="3D Models" models={filteredModel} />
+        </>
+    )
 }
